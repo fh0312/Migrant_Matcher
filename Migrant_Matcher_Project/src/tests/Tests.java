5e12157897e572e011523b05dee610e046b4d011
hiperfrance <fc56348@alunos.fc.ul.pt>
2022-05-24 16:19:35 +0100

    Trying Java
    Nothing happened

---
+++ Migrant_Matcher_Project/src/tests/Tests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Tests {
+
+	@Test
+	void testPass() {
+		assertEquals('a', 'a');
+	}
+
+
+}
