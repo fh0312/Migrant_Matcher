d0a271aa1b93041d5ff609a59924f58a4a9c8a69
hiperfrance <fc56348@alunos.fc.ul.pt>
2022-05-29 17:37:12 +0100

    Update Tests.java
    Novos testes com SysOuts (nao definitivos) para testar as classes Regioes, e Ajuda com subclasses e catalogos incluidos.

--- Migrant_Matcher_Project/src/tests/Tests.java
+++ Migrant_Matcher_Project/src/tests/Tests.java
@@ -4,19 +4,72 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import utilizador.Migrante;
+import ajuda.Ajuda;
+import ajuda.Alojamento;
+import ajuda.CatalogoAjudas;
+import ajuda.Item;
+import regiao.CatalogoRegioes;
+import regiao.Regiao;
 
 class Tests {
 
 	@Test
-	void testPass() {
-		Migrante m1 = new Migrante("Antonio",112);
-		//Migrante m2 = new Migrante (4);
-		assertEquals("Antonio",m1.getNome());
-		assertEquals(112,m1.getTel());
-//		assertEquals("Joao",m2.getName());
-//		assertEquals(111,m2.getTel());
+	void testRegioes() {
+		Regiao r1 = new Regiao("Viseu");
+		Regiao r2 = new Regiao("Lisboa");
+		CatalogoRegioes catR = new CatalogoRegioes();
+		catR.adicionaRegiao(r1);
+		catR.adicionaRegiao(r2);
+		for(Regiao r : catR.getRegioes()) {
+			System.out.println(r.getNome());
+		}
+		System.out.println("Regiao r2 = "+catR.getRegiao(r2).getNome());
+		assertEquals(catR.getRegiao(r1), r1);
+		assertEquals(catR.getRegiao(r2), r2);
 	}
+	
+	@Test
+	void testAjudas() {
+		Regiao viseu = new Regiao("Viseu");
+		Regiao lisboa = new Regiao("Lisboa");
+		Ajuda aloj1 = new Alojamento(4, viseu );
+		Ajuda aloj2 = new Alojamento(2, viseu );
+		Ajuda aloj3 = new Alojamento(4, lisboa );
+		Ajuda aloj4 = new Alojamento(3, lisboa );
+		Ajuda aloj5 = new Alojamento(4, viseu );
+		Ajuda item1 = new Item("brinquedo");
+		Ajuda item2 = new Item("manta");
+		Ajuda item3 = new Item("peluche");
+		
+		CatalogoAjudas catA = new CatalogoAjudas();
+		catA.adicionaAjuda(aloj1);
+		catA.adicionaAjuda(aloj2);
+		catA.adicionaAjuda(aloj3);
+		catA.adicionaAjuda(aloj4);
+		catA.adicionaAjuda(aloj5);
+		catA.adicionaAjuda(item1);
+		catA.adicionaAjuda(item2);
+		catA.adicionaAjuda(item3);
+
+		for(Ajuda a : catA.getAjudas()) {
+			System.out.println(a.getNome());
+		}
+		System.out.println("Ajuda aloj3 = " + catA.getAjuda(aloj3).getNome());
+		assertEquals(catA.getAjuda(aloj1), aloj1);
+		assertEquals(catA.getAjuda(aloj3), aloj3);
+		System.out.println("Removendo ajuda...");
+		catA.removeAjuda(aloj5);
+		for(Ajuda a : catA.getAjudas()) {
+			System.out.println(a.getNome());
+		}
+		System.out.println("Removendo ajuda...");
+		catA.removeAjuda(aloj1);
+		for(Ajuda a : catA.getAjudas()) {
+			System.out.println(a.getNome());
+		}
+		
+	}
+
 
 
 }
