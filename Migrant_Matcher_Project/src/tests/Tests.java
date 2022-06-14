7bcd13b21769b4f82085ea9ebac9499e2b61cab5
mstcardoso <mst.cardoso94@gmail.com>
2022-06-14 17:11:42 +0100

    Update Tests.java
    finalizado o teste testRegioes

--- Migrant_Matcher_Project/src/tests/Tests.java
+++ Migrant_Matcher_Project/src/tests/Tests.java
@@ -2,25 +2,19 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
-import main.MigrantMatcher;
 import ajuda.Ajuda;
 import ajuda.Alojamento;
 import ajuda.CatalogoAjudas;
 import ajuda.Item;
-import ajuda.sms.EnviadoresSMS;
 import handlers.RegistaMigranteHandler;
 import pedido_ajuda.PedidoAjuda;
 import regiao.CatalogoRegioes;
 import regiao.Regiao;
-import utilizador.Familiar;
 import utilizador.Migrante;
 import utilizador.Voluntario;
 
@@ -29,60 +23,20 @@ class Tests {
 	private int telMigrante1 = 938933680;
 	private int telVoluntario = 123456789;
 	
-//	@Test
-//	void testRegioes() {
-//		Regiao r1 = new Regiao("Viseu");
-//		Regiao r2 = new Regiao("Lisboa");
-//		CatalogoRegioes catR = new CatalogoRegioes();
-//		catR.adicionaRegiao(r1);
-//		catR.adicionaRegiao(r2);
-//		for(Regiao r : catR.getRegioes()) {
-//			System.out.println(r.getNome());
-//		}
-//		System.out.println("Regiao r2 = "+catR.getRegiao(r2).getNome());
-//		assertEquals(catR.getRegiao(r1), r1);
-//		assertEquals(catR.getRegiao(r2), r2);
-//	}
-//	
-//	@Test
-//	void testAjudas() {
-//		Regiao viseu = new Regiao("Viseu");
-//		Regiao lisboa = new Regiao("Lisboa");
-//		Ajuda aloj1 = new Alojamento(4, viseu );
-//		Ajuda aloj2 = new Alojamento(2, viseu );
-//		Ajuda aloj3 = new Alojamento(4, lisboa );
-//		Ajuda aloj4 = new Alojamento(3, lisboa );
-//		Ajuda aloj5 = new Alojamento(4, viseu );
-//		Ajuda item1 = new Item("brinquedo");
-//		Ajuda item2 = new Item("manta");
-//		Ajuda item3 = new Item("peluche");
-//		
-//		CatalogoAjudas catA = new CatalogoAjudas();
-//		catA.adicionaAjuda(aloj1);
-//		catA.adicionaAjuda(aloj2);
-//		catA.adicionaAjuda(aloj3);
-//		catA.adicionaAjuda(aloj4);
-//		catA.adicionaAjuda(aloj5);
-//		catA.adicionaAjuda(item1);
-//		catA.adicionaAjuda(item2);
-//		catA.adicionaAjuda(item3);
-//
-//		for(Ajuda a : catA.getAjudas()) {
-//			System.out.println(a.getNome());
-//		}
-//		System.out.println("Ajuda aloj3 = " + catA.getAjuda(aloj3).getNome());
-//		assertEquals(catA.getAjuda(aloj1), aloj1);
-//		assertEquals(catA.getAjuda(aloj3), aloj3);
-//		System.out.println("Removendo ajuda...");
-//		catA.removeAjuda(aloj5);
-//		for(Ajuda a : catA.getAjudas()) {
-//			System.out.println(a.getNome());
-//		}
-//		System.out.println("Removendo ajuda...");
-//		catA.removeAjuda(aloj1);
-//		for(Ajuda a : catA.getAjudas()) {
-//			System.out.println(a.getNome());
-//		}
+	@Test
+	void testRegioes() {
+		Regiao r1 = new Regiao("Madrid");
+		Regiao r2 = new Regiao("Paris");
+		CatalogoRegioes catR = new CatalogoRegioes();
+		catR.adicionaRegiao(r1);
+		catR.adicionaRegiao(r2);
+		for(Regiao r : catR.getRegioes()) {
+			System.out.println(r.getNome());
+		}
+		System.out.println("Regiao r2 = "+catR.getRegiao(r2.getNome()));
+		assertEquals(r1, catR.getRegiao(r1.getNome()));
+		assertEquals(r2, catR.getRegiao(r2.getNome()));
+	}
 	
 	@Test
 	void addAjuda() {
@@ -121,9 +75,6 @@ class Tests {
 		catA.adicionaAjuda(item3);
 		
 		List<Ajuda> lista = catA.getAjudas();
-		for(Ajuda a: lista) {
-		System.out.println(a.getNome());
-		}
 		
 		migrante.setPaCorrente(new PedidoAjuda(catA));
 		migrante.getPaCorrente().adicionaAjuda(lista.get(1));
@@ -133,9 +84,6 @@ class Tests {
 		migrante.adicionaPedido(migrante.getPaCorrente());
 		
 		List<Ajuda> listAjudas = migrante.getListaPedidos().get(0).getAjudasPedidas();
-		for(Ajuda a: listAjudas) {System.out.println();
-			System.out.println(a.getNome());
-			}
 		
 		assertEquals(3, catA.getAjudas().size()); // confirmar que as ajudas pedidas foram retiradas do catálogo
 		assertEquals("Alojamento_LISBOA_5", listAjudas.get(0).getNome());
