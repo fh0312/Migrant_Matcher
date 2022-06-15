277647d5995636197bf917c9d29d1abef1327cf0
mstcardoso <mst.cardoso94@gmail.com>
2022-06-15 18:17:38 +0100

    Javadocs
    Javadocs dos metodos das Classes CatalogoRegioes e Regiao

--- Migrant_Matcher_Project/src/regiao/CatalogoRegioes.java
+++ Migrant_Matcher_Project/src/regiao/CatalogoRegioes.java
@@ -8,6 +8,9 @@ public class CatalogoRegioes {
 	
 	private HashMap<String,Regiao> regioesDisponiveis;
 	
+	/**
+	 * Construtor de CatalogoRegioes, cria um HashMap para guardar regioes, inicializando este ja com algumas regioes
+	 */
 	public CatalogoRegioes() {
 		this.regioesDisponiveis = new HashMap<>();
 		String regioes = "lisboa,porto,setubal,braga,aveiro,faro,leiria,santarem,"
@@ -17,21 +20,41 @@ public class CatalogoRegioes {
 		}
 	}
 	
+	/**
+	 * Adiciona uma Regiao ao sistema
+	 * @param r uma Regiao
+	 */
 	public void adicionaRegiao(Regiao r) {
 		this.regioesDisponiveis.put(r.getNome().toLowerCase(), r);
 	}
 	
+	/**
+	 * Remove uma Regiao do sistema
+	 * @param r uma Regiao
+	 */
 	public void removeRegiao(Regiao r) {
 		this.regioesDisponiveis.remove(r.getNome().toLowerCase());
 	}
 	
+	/**
+	 * Devolve uma regiao atraves do seu nome
+	 * @param nome o nome de uma Regiao
+	 * @return uma Regiao
+	 */
 	public Regiao getRegiao(String nome) {
 		return this.regioesDisponiveis.get(nome.toLowerCase());
 	}
+	/**
+	 * Devolve umaa lista com as regioes disponiveis
+	 * @return uma lista com as regioes disponiveis
+	 */
 	public List<Regiao> getRegioes(){
 		return new ArrayList<>(this.regioesDisponiveis.values());
 	}
 	
+	/**
+	 * Passa para String a lista de Regioes
+	 */
 	@Override
 	public String toString() {
 		StringBuilder result = new StringBuilder();
